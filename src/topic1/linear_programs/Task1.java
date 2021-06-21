726fba51da962d5ad104d2b859ec4de72cea7656
natal-lia-b <80335618+natal-lia-b@users.noreply.github.com>
2021-06-22 01:37:02 +0300

    task is done

--- src/topic1/linear_programs/Task1.java
+++ src/topic1/linear_programs/Task1.java
@@ -1,7 +1,14 @@
 package topic1.linear_programs;
 
+// 1. Найдите значение функции: z = ( (a – 3 ) * b / 2) + c
+
 public class Task1 {
+    public static void main(String[] args) {
+        int z = calculateTask1(13, 10, 5);  // value = 55
+        System.out.println("value = " + z);
+    }
 
-
-
+    public static int calculateTask1(int a, int b, int c) {
+        return (a - 3) * b / 2 + c;
+    }
 }
