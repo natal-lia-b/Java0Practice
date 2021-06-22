4216c3347b208e4d0bb188614b720763e4f35d86
natal-lia-b <80335618+natal-lia-b@users.noreply.github.com>
2021-06-22 19:36:12 +0300

    cycles. task8 is in progress.

---
+++ src/topic1/part3_cycles/Task8.java
@@ -0,0 +1,13 @@
+package topic1.part3_cycles;
+
+/*8. Даны два числа. Определить цифры, входящие в запись как первого так и второго числа.*/
+
+public class Task8 {
+    public static void main(String[] args) {
+        defineEqualNumerals(12569, 851);
+    }
+
+    public static void defineEqualNumerals(int a, int b) {
+
+    }
+}
