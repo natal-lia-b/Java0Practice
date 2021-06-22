3f9444db60e8e3dd1f8da9930bd3a01fd47efaa7
natal-lia-b <80335618+natal-lia-b@users.noreply.github.com>
2021-06-22 08:58:24 +0300

    task 4 is done

---
+++ src/topic1/linear_programs/Task4.java
@@ -0,0 +1,16 @@
+package topic1.linear_programs;
+
+/*4. Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях).
+Поменять местами дробную и целую части числа и вывести полученное значение числа.*/
+
+public class Task4 {
+    public static void main(String[] args) {
+        swapIntegerAndFractionalParts(111.555);
+    }
+
+    public static void swapIntegerAndFractionalParts(double x) {
+        int integerPart = (int) x;
+        int fractionalPart = (int) ((x - integerPart) * 1000);
+        System.out.println(fractionalPart + (double) integerPart / 1000);
+    }
+}
