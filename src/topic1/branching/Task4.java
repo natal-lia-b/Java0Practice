8c939246db21bc3b36df8c15f20e36207c54d4bf
natal-lia-b <80335618+natal-lia-b@users.noreply.github.com>
2021-06-22 13:18:49 +0300

    branching. task 4 is done

---
+++ src/topic1/branching/Task4.java
@@ -0,0 +1,21 @@
+package topic1.branching;
+
+/*4. Заданы размеры А, В прямоугольного отверстия и размеры х, у, z кирпича.
+Определить, пройдет ли кирпич через отверстие.*/
+
+public class Task4 {
+    public static void main(String[] args) {
+        checkBrick(4, 9, 10, 2, 3);  // yes
+        checkBrick(4, 9, 10, 12, 5);  // no
+    }
+
+    public static void checkBrick(int a, int b, int x, int y, int z) {
+        if ((a >= x && b >= y) || (a >= y && b >= x) ||
+                (a >= x && b >= z) || (a >= z && b >= x) ||
+                (a >= y && b >= z) || (a >= z && b >= y))  {
+            System.out.println("The brick can be gone through the hole");
+        } else {
+            System.out.println("The brick can't be gone through the hole");
+        }
+    }
+}
