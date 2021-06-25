b4881f4ca4aff1c23cc2edf77200d13e6da9b217
natal-lia-b <80335618+natal-lia-b@users.noreply.github.com>
2021-06-25 14:57:59 +0300

    cycles. task8 is done.

--- src/topic1/part3_cycles/Task8.java
+++ src/topic1/part3_cycles/Task8.java
@@ -5,9 +5,30 @@ package topic1.part3_cycles;
 public class Task8 {
     public static void main(String[] args) {
         defineEqualNumerals(12569, 851);
+        defineEqualNumerals(11177, 1717);
     }
 
     public static void defineEqualNumerals(int a, int b) {
+        int minNumber = a;
+        int maxNumber = b;
+        if (a > b) {
+            minNumber = b;
+            maxNumber = a;
+        }
 
+        while (minNumber > 0) {
+            int lastDigit = minNumber % 10;
+            minNumber = minNumber / 10;
+
+            int maxNumberCopy= maxNumber;
+            while (maxNumberCopy > 0) {
+                if (maxNumberCopy % 10 == lastDigit) {
+                    System.out.print(lastDigit);
+                    break;
+                }
+                maxNumberCopy = maxNumberCopy / 10;
+            }
+        }
+        System.out.println();
     }
 }
