aad60a99ece697984295cdf2ec0d151045a9641e
natal-lia-b <80335618+natal-lia-b@users.noreply.github.com>
2021-06-29 22:51:39 +0300

    part1_matrixes. task2 is done.

--- src/topic2/part2_matrixes/Task2.java
+++ src/topic2/part2_matrixes/Task2.java
@@ -1,4 +1,7 @@
 package topic2.part2_matrixes;
+/**
+ * 2. Дана квадратная матрица. Вывести на экран элементы, стоящие на диагонали.
+ */
 
 import java.util.Scanner;
 
@@ -6,29 +9,35 @@ public class Task2 {
     static Scanner scanner;
     static int[][] matrix;
 
-//    public static void main(String[] args) {
-//        initiateMatrix();
-//        fillMatrix();
-//        printMatrix();
-//    }
-//    static void printMatrix() {
-//        System.out.print("New array:" + Arrays.toString(matrix));
-//    }
-//    private static void initiateMatrix() {
-//        scanner = new Scanner(System.in);
-//        System.out.println("Enter matrix dimension N and press <Enter>:");
-//        int n = scanner.nextInt();
-//        System.out.println("Enter matrix dimension M and press <Enter>:");
-//        int m = scanner.nextInt();
-//        matrix = new int[n][m];
-//    }
-//
-//    static void fillMatrix() {
-//        for (int i = 0; i < matrix.length; i++) {
-//            for( int j = 0; j < matrix[0].length; j++) {
-//                System.out.println("Enter matrix[" + i + "][" + j + "] :");
-//                matrix[i][j] = scanner.nextInt();
-//            }
-//        }
-//    }
+    public static void main(String[] args) {
+        initiateMatrix();
+        fillMatrix();
+        printMatrix();
+    }
+
+    static void printMatrix() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == j) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+            }
+        }
+    }
+
+    private static void initiateMatrix() {
+        scanner = new Scanner(System.in);
+        System.out.println("Enter matrix dimension and press <Enter>:");
+        int n = scanner.nextInt();
+        matrix = new int[n][n];
+    }
+
+    static void fillMatrix() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.println("Enter element[" + i + "][" + j + "]:");
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+    }
 }
