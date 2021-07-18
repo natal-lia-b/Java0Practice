0428eedeb5ba7de18e0046ff670739df52ee1c2c
natal-lia-b <80335618+natal-lia-b@users.noreply.github.com>
2021-07-18 03:21:16 +0300

    part3_RegEx. task2 - raw done.

--- src/topic3/part3_RegEx/Task2.java
+++ src/topic3/part3_RegEx/Task2.java
@@ -1,5 +1,6 @@
 package topic3.part3_RegEx;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -32,7 +33,7 @@ public class Task2 {
                 "       <from>Света</from>\n" +
                 "       <heading>Напоминание</heading>\n" +
                 "       <body>Позвони мне завтра!</body>\n" +
-                "   </note>\n" +
+//                "   </note>\n" +
                 "   <note id = \"2\">\n" +
                 "       <to>Петя</to>\n" +
                 "       <from>Маша</from>\n" +
@@ -44,32 +45,79 @@ public class Task2 {
     }
 
     private static String parse(String xmlText) {
-        Pattern patternOpenedTag = Pattern.compile("<\\w+?>");
+        Pattern patternOpenedTag = Pattern.compile("<\\w+");
+        Pattern patternContent = Pattern.compile(">.+?<");
+        Pattern patternAttributes = Pattern.compile("<\\w+(\\s+\\w+)+");
 
-        StringBuilder result = new StringBuilder();
         String[] lines = xmlText.split("\n");
+        boolean isWrongFormat = false;
+        StringBuilder result = new StringBuilder();
+        StringBuilder resultWrongFormat = new StringBuilder();
+        ArrayList<Integer> closedTagsIndexes = new ArrayList<>();
 
-//        for (String line : lines) {
         for (int i = 0; i < lines.length; i++) {
             Matcher matcherOpenedTag = patternOpenedTag.matcher(lines[i]);
             if (matcherOpenedTag.find()) {
                 String name = matcherOpenedTag.group();
-                result.append(name).append(" - opened tag.\n");
+                result.append(name).append("> - tag is opened.\n");
 
-                Pattern patternClosedTag = Pattern.compile("</"+ name.replaceAll("<", ""));
+//                Pattern patternWrongFormat = Pattern.compile("(" + name + "){1}(^[\\s>])+");
+                Pattern patternWrongFormat = Pattern.compile(name + "/");
+                Matcher matcherWrongFormat = patternWrongFormat.matcher(lines[i]);
+                if (matcherWrongFormat.find()) {
+                    isWrongFormat = true;
+                    resultWrongFormat.append("Line".concat((i + 1) + ". Wrong tag format.\n"));
+                    continue;
+                }
+
+                Matcher matcherAttributes = patternAttributes.matcher(lines[i].strip());
+                if (matcherAttributes.find()) {
+                    String attributes = lines[i].strip().replaceAll(name, "").replaceAll("[<>/]", "").strip();
+                    result.append(attributes).append(" - attributes.\n");
+                }
+
+                Pattern patternClosedTag = Pattern.compile("</" + name.replaceAll("<", ""));
                 int j = i;
+                StringBuilder content = new StringBuilder();
+                boolean isClosed = false;
                 while (j < lines.length) {
                     Matcher matcherClosedTag = patternClosedTag.matcher(lines[j]);
                     if (matcherClosedTag.find()) {
-                        result.append(matcherClosedTag.group()).append(" - closed tag.\n");
+                        if (i == j) {
+                            Matcher matcherContent = patternContent.matcher(lines[j]);
+                            if (matcherContent.find()) {
+                                result.append(matcherContent.group(), 1, matcherContent.group().length() - 1);
+                            }
+                        } else {
+                            result.append(content);
+                        }
+                        if (closedTagsIndexes.contains(j)) {
+                            isClosed = false;
+                        } else {
+                            closedTagsIndexes.add(j);
+                            isClosed = true;
+                        }
+                        result.append(" - content.\n");
+                        result.append(matcherClosedTag.group()).append("> - tag is closed.\n");
                         break;
                     }
-                    result.append(lines[j]);
+                    if (i != j) {
+                        content.append(lines[j].concat("\n"));
+                    }
                     j++;
                 }
-
+                if (!isClosed) {
+                    isWrongFormat = true;
+                    resultWrongFormat.append("Line".concat((i + 1) + ". Opened tag is not closed.\n"));
+                }
+            } else {
+                if (!closedTagsIndexes.contains(i)) {
+                    isWrongFormat = true;
+                    resultWrongFormat.append("Line".concat((i + 1) + ". Opened tag is not found.\n"));
+                }
             }
+
         }
-        return  result.toString();
+        return isWrongFormat ? resultWrongFormat.toString() : result.toString();
     }
 }
