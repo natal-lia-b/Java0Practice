6779bc877deb944f9b2749926b01f7c51907b47e
natal-lia-b <80335618+natal-lia-b@users.noreply.github.com>
2021-07-21 16:56:26 +0300

    part1_SimpleClasses. task7 - methods added.

--- src/topic4/part1_SimpleClasses/Triangle.java
+++ src/topic4/part1_SimpleClasses/Triangle.java
@@ -17,6 +17,7 @@ public class Triangle {
 
     public static void main(String[] args) {
         Triangle triangle = new Triangle(0, 0, 1, 2, 2, 1);
+        System.out.println(triangle.toString());
         System.out.printf("Perimeter = %8.4f\n", triangle.perimeter());
         System.out.printf("Square    = %8.4f\n", triangle.square());
         double[] meridianCoordinates = triangle.medianCoordinates();
@@ -66,4 +67,63 @@ public class Triangle {
         return correctSides;
     }
 
+    @Override
+    public String toString() {
+        return "Triangle {" +
+                "dot1(" + x1 +
+                ", " + y1 +
+                "), dot2(" + x2 +
+                ", " + y2 +
+                "), dot3(" + x3 +
+                ", " + y3 +
+                ")}";
+    }
+
+    public double getX1() {
+        return x1;
+    }
+
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    public double getY1() {
+        return y1;
+    }
+
+    public void setY1(double y1) {
+        this.y1 = y1;
+    }
+
+    public double getX2() {
+        return x2;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public double getY2() {
+        return y2;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
+    }
+
+    public double getX3() {
+        return x3;
+    }
+
+    public void setX3(double x3) {
+        this.x3 = x3;
+    }
+
+    public double getY3() {
+        return y3;
+    }
+
+    public void setY3(double y3) {
+        this.y3 = y3;
+    }
 }
